11fa4472b247605f3cdb9276af368648f1a0316b
Maxime Bret <Maxime Bret>
2016-03-19 00:11:13 +0100

    no message

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-exports.logger = require('./lib/logger');
+exports.logger      = require('./lib/logger');
+exports.scheduler   = require('./lib/scheduler');
+exports.utils       = require('./lib/utils');
+exports.taskQueue   = require('./lib/task-queue');
+exports.requireAll  = require('./lib/require-all');
