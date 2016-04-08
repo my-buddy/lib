be81cc4c4ba72bd3bc9a18d7339f4117c7db920f
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-04-08 14:00:01 +0200

    no message

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ exports.scheduler   = require('./lib/scheduler');
 exports.utils       = require('./lib/utils');
 exports.taskQueue   = require('./lib/task-queue');
 exports.requireAll  = require('./lib/require-all');
-exports.winstonTransportSequelize  = require('./lib/winston-transport-sequelize');
+exports.WinstonTransportSequelize  = require('./lib/winston-transport-sequelize');
